8918c5f55660e0d298490fe57ff03090e316d25c
reskyadhyaksa <reskyadhyaksa19@gmail.com>
2026-01-23 23:59:00 +0700

    Removing Title

--- src/app/experiences/page.js
+++ src/app/experiences/page.js
@@ -1,11 +1,7 @@
 export default function ExperincePages() {
   return (
     <div className="bg-blue relative min-h-screen w-full place-items-center bg-blue-950 text-center text-9xl text-white">
-      <div class="relative h-full w-full bg-black">
-        <div class="absolute top-0 right-0 bottom-0 left-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
-        <div class="absolute top-[-10%] right-0 left-0 h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div>
-      </div>
+      
     </div>
   );
 }
- 
